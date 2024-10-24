7afd9d2b50a7ae00e83e53ca9a86b5c0de5517af
syedMohiuddin6 <mohiuddinrx6@gmail.com>
2024-10-24 17:22:28 +0500

    first commit

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		console.log(`MongoDB connected: ${conn.connection.host}`);
 	} catch (error) {
-		console.log("Error connecting to MONGODB", error.message);
+		console.log("Error connecting to MONGODB:", error.message);
 		process.exit(1);
 	}
 };
